7b97455487b95c6720155a37d14d78548ca0c0a4
NadavRuni <156665035+NadavRuni@users.noreply.github.com>
2024-01-24 13:40:30 +0200

    1

--- GenThree.java
+++ GenThree.java
@@ -4,30 +4,25 @@
  */
 public class GenThree {
 
+	public static void main(String[] args) {
+		int c = Integer.parseInt(args[0]);
+		int d = Integer.parseInt(args[1]);
 
-		public static void main(String[] args) {
-			int c=Integer.parseInt(args[0]); 
-			int d=Integer.parseInt(args[1]); 
-	
-			int i=0, a=0, mini=d;
-			double rand;
-			
-		  
-	
-			 
-			while (i!=3) // לולאה שרצה 3 פעמים 
-			{
-				rand=(Math.random()*(d-c+1))+c;// מגרילה מספר בין c ל d 
-				a =(int) rand;
-				System.out.println(a);
-				if (a<mini)
-				{
-					mini = a; 
-				}
-				
-				i++;
+		int i = 0, a = 0, mini = d;
+		double rand;
+
+		while (i != 3) // לולאה שרצה 3 פעמים
+		{
+			rand = (Math.random() * (d - c + 1)) + c;// מגרילה מספר בין c ל d
+			a = (int) rand;
+			System.out.println(a);
+			if (a < mini) {
+				mini = a;
 			}
-			System.out.println("the minimL generated number was "+ mini);
-			
+
+			i++;
+		}
+		System.out.println("The minimL generated number was " + mini);
+
 	}
 }
